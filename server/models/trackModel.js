7c97a1983d0d1c430777c8dccb0603d58e7f8d22
Levan Khunjgurua <l.khunjgurua@gmail.com>
2017-07-15 23:52:31 +0200

    unnecessary files removed

--- server/models/trackModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var trackModel = new Schema({
-    name: { type: String },
-    artists: String
-});
-
-module.exports = mongoose.model('Tracks', trackModel);
