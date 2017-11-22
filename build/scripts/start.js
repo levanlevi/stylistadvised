928b1860db8d7740dc97766f86109986740d83c8
Gurfinkel <flatorez@gmail.com>
2017-11-22 21:22:07 +0300

    Added socket.io and socket.io-client.

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,21 @@
-const logger = require('../lib/logger')
+const logger = require('../lib/logger');
 
-logger.info('Starting server...')
-require('../../server/main').listen(3000, () => {
+logger.info('Starting server...');
+const server = require('../../server/main').listen(3000, () => {
   logger.success('Server is running at http://localhost:3000')
-})
+});
+
+const connections = [];
+
+const io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+  
+  socket.once('disconnect', function() {
+    connections.splice(connections.indexOf(socket), 1);
+    socket.disconnect();
+    console.log("Disconnected: %s sockets remaining.", connections.length);
+  });
+
+  connections.push(socket);
+  console.log("Connected: %s sockets connected.", connections.length);
+});
--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,35 @@ import { browserHistory, Router } from 'react-router';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 
+var io = require('socket.io-client');
+
 class App extends React.Component {
   static propTypes = {
     store: PropTypes.object.isRequired,
     routes: PropTypes.array.isRequired,
+  }
+
+  componentWillMount() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', this.connect);
+    this.socket.on('disconnect', this.disconnect);
+  }
+
+  componentDidMount() {
+    // initialization of HSDropdown component
+    $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {
+      afterOpen: function () {
+        $(this).find('input[type="search"]').focus();
+      }
+    });
+  }
+
+  connect() {    
+    console.log('connected');
+  }
+
+  disconnect() {
+    console.log('disconnected');
   }
 
   shouldComponentUpdate () {
